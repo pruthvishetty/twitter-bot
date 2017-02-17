25290483b22cb004c1d6ea57e678cbbec72ae3d6
Pruthvi N. Shetty <pruthvi1124@gmail.com>
2017-02-17 13:52:13 -0500

    Add files via upload

---
+++ twitter_auth.py
@@ -0,0 +1,14 @@
+
+#configuration
+MY_CONSUMER_KEY = '*'
+MY_CONSUMER_SECRET = '*'
+MY_ACCESS_TOKEN_KEY = '*'
+MY_ACCESS_TOKEN_SECRET = '*'
+
+SOURCE_ACCOUNTS = ["one","two","three","four"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+ODDS = 1 #How often do you want this to run? 1/8 times?
+ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
+DEBUG = False #Set this to False to start Tweeting live
+STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
+TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
+TWEET_ACCOUNT = "yourusername" #The name of the account you're tweeting to.
